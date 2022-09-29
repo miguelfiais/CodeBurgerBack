2acd0ec643fc1eff6ce2e05dd121f809e53411e6
Miguel Fiais <miguel.fiais10@gmail.com>
2022-09-29 19:12:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.json())
+const port = 3002
+app.use(cors())
+const { v4: uuidv4 } = require('uuid');
+
+const orders = []
+
+app.post('/orders', (request, response) => {
+    const {burger, name} = request.body
+    const newOrder = {id: uuidv4(), burger, name}
+    orders.push(newOrder)
+    return response.status(201).json(newOrder)
+})
+app.get('/orders',(request, response)=> {
+    const index = orders.findIndex(order => order.burger === "Selecione um pedido" || order.name === "")
+    if(index < 0 ){
+        return response.json(orders)
+    }
+    orders.splice(index,1)
+    return response.json(orders)
+    
+})
+app.delete('/orders/:id',(request, response)=> {
+    const {id} = request.params
+    const index = orders.findIndex(order => id===order.id)
+    if(index < 0) {
+        return response.status(404).json("Order not found")
+    }
+    orders.splice(index,1)
+    return response.status(204).json()
+    
+})
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`)
+})
